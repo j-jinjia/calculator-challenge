958d3a5dff5bb9b2644cee405cbb0bf8cfb283aa
j-jinjia <j.jinjia99@gmail.com>
2022-02-17 17:34:47 +0100

    Solved bugs on operation function and AC button

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //global variables
-let number1="0";
-let number2="0"
+let number1="";
+let number2=""
 let operator;
 let result;
 
 let numberButtons = document.querySelectorAll(".calculator__number");
 let operationButtons = document.querySelectorAll(".calculator__operator");
-let restartButton = document.querySelector(".calculator__restart");
-const screenResult = document.getElementsByClassName("display__result");
+const restartButton = document.querySelector(".calculator__restart");
+const screenResult = document.querySelector(".display__result");
 const screenOperation = document.querySelector(".display__operation")
 const equalButton = document.querySelector(".calculator__equal")
 
@@ -15,7 +15,7 @@ const equalButton = document.querySelector(".calculator__equal")
 
 const onNumberButtonsClick = (event) => {
     screenOperation.innerText += event.target.innerText;
-    number1=event.targe.innerText;
+    number1+=event.target.innerText;
 };
 for (let index = 0; index < numberButtons.length; index++) {
     numberButtons[index].addEventListener("click", onNumberButtonsClick);
@@ -25,53 +25,59 @@ for (let index = 0; index < numberButtons.length; index++) {
 /**operationButton */
 const onOperationButtonsClick = (event) => {
     screenOperation.innerText += event.target.innerText;
+    number2 = number1;
+    number1="";
+    operator=event.target.innerText;
 };
 for (let index = 0; index < operationButtons.length; index++) {
     operationButtons[index].addEventListener("click", onOperationButtonsClick);
 };
-/** AC button (restart). Defined function of clearing the screen display when AC button is clicked by adding event listener to the button. */
-restartButton.addEventListener("click", (onRestartButtonClick));
 
-const onRestartButtonClick = (event) => {
-    screenOperation.innerHTML="";
-    screenResult.innerText="";
-    console.log("hello")
-    //screenOperation.innerHTML = clear(number1, number2, operator);
-    //screenResult.innerHTML = clear(number1, number2, operator);
 
-};
-/**const clear = (number1, number2, operator) => {
-    if (number1 || number2 || operator) {
-      return "";
-    }
-  }*/
+
 /**Equal button (=): On click, performs calculate function and checks for which operator is being used to perform the calculation. Then it displays on screen the result and clears the operation. */
+
+
+const operation = (number1, number2, operator) => {
+
+    if (typeof number1 !== "number" || typeof number2 !== "number" ){
+        number1= Number(number1);
+        number2=Number(number2);
+    }
+    if (operator === "+" ) {
+        return number1 + number2;
+    } else if (operator === "-"){
+        return number1 - number2;
+    } else if (operator === "x") {
+        return number1 * number2;
+    } else if (operator === "÷") {
+        return number2 / number1;
+    } else if (operator === "%") {
+        return number1 / 100 || number2 / 100;
+    } else {
+        return;
+    }
+    
+};
 equalButton.addEventListener('click', (event)=> {
     screenResult.innerHTML = operation(number1, number2, operator);
-    //screenOperation.innerHTML = clear(number1, number2, operator);
+    //screenOperation.innerHTML ="";
     
     
 });
-const operation = (number1, number2, operator) => {
-    if (operator === "+") {
-        return Number(number1) + Number(number2);
-    } else if (operator === "-") {
-        const result = parseInt(number) - parseInt(number2);
-      return result
-    } else if (operator === "x") {
-      return parseInt(number1) * parseInt(number2);
-    } else if (operator === "÷") {
-        const result = parseInt(number1) / parseInt(number2);
-      return result 
-    } else if (operator === "%") {
-      return parseInt(number1) / 100 || parseInt(number2) / 100;
-    } else {
-      return;
-    }
-  
-}
-  
-  
-  
 
+/** AC button (restart). Defined function of clearing the screen display when AC button is clicked by adding event listener to the button. */
+const onRestartButtonClick = (event) => {
+    screenOperation.innerText ="";
+    screenResult.innerText="";
+    number1="";
+    number2="";
+    operator="";
+};
+    
+restartButton.addEventListener("click", onRestartButtonClick);
 
+    
+    
+    
+    
